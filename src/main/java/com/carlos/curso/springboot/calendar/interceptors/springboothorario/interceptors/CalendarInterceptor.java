1824e3731d0d74ae2060b78c17a73a4ecef3aabc
Carlos <carlosicaraballo1999@gmail.com>
2025-02-04 14:54:30 +0000

    100. Implementando cerrado en el interceptor

--- src/main/java/com/carlos/curso/springboot/calendar/interceptors/springboothorario/interceptors/CalendarInterceptor.java
+++ src/main/java/com/carlos/curso/springboot/calendar/interceptors/springboothorario/interceptors/CalendarInterceptor.java
@@ -1,22 +1,59 @@
 package com.carlos.curso.springboot.calendar.interceptors.springboothorario.interceptors;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class CalendarInterceptor implements HandlerInterceptor {
+
+  private Logger logger = LoggerFactory.getLogger(CalendarInterceptor.class);
 
   @Value("${config.calendar.open}")
   private Integer open;
 
   @Value("${config.calendar.close}")
   private Integer close;
+
+  private void inTime(HttpServletRequest request) {
+    StringBuilder message = new StringBuilder("Bienvenidos al horario de atencion al cliente");
+    message.append(", atendemos desde las ");
+    message.append(this.open);
+    message.append("hrs. hasta las ");
+    message.append(this.close);
+    message.append("hrs. Gracias por su visita");
+    request.setAttribute("message", message.toString());
+  }
+
+  private void outTime(HttpServletResponse response) throws IOException {
+    Map<String, Object> data = new HashMap<>();
+    StringBuilder message = new StringBuilder("Cerrado, fuera del horario de atención por favor visitenos desde las");
+
+    message.append(this.open);
+    message.append(" y las ");
+    message.append(this.close);
+    message.append(" hrs. Gracias!");
+
+    data.put("message", message.toString());
+
+    response.setContentType("application/json");
+    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+    response.getWriter().write(new ObjectMapper().writeValueAsString(data));
+  }
 
   @Override
   public boolean preHandle(
@@ -26,18 +63,13 @@ public class CalendarInterceptor implements HandlerInterceptor {
   ) throws Exception {
     Calendar calendar = Calendar.getInstance();
     int hour = calendar.get(Calendar.HOUR_OF_DAY);
-    
+    logger.info(Integer.toString(hour));
+
     if (hour >= this.open && hour < this.close) {
-      StringBuilder message = new StringBuilder("Bienvenidos al horario de atencion al cliente");
-      message.append(", atendemos desde las ");
-      message.append(this.open);
-      message.append("hrs. hasta las ");
-      message.append(this.close);
-      message.append("hrs. Gracias por su visita");
-      request.setAttribute("message", message.toString());
+      this.inTime(request);
       return true;
     }
-
+    this.outTime(response);
     return false;
 
   }
